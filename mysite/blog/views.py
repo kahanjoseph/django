ce351dccae3c8629a7d8f1ae60bf751b6b229e1d
Joseph Kahan <kahanjoseph@gmail.com>
2024-09-12 16:16:18 -0400

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from .models import Post
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
