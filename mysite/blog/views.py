7806c942d0075bc62e04585b304b6e998da824cd
Joseph Kahan <kahanjoseph@gmail.com>
2024-09-13 09:46:54 -0400

    commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 from .models import Post
 
@@ -49,4 +50,30 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        # Form fields passed validation
+        cd = form.cleaned_data
+    # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
     )
