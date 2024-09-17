7f390086d035e2108b3507f8c3bdf26014e35bc9
Joseph Kahan <kahanjoseph@gmail.com>
2024-09-17 16:29:18 -0400

    .

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
  # post views
  # path('', views.post_list, name='post_list'),
  path('', views.PostListView.as_view(), name='post_list'),
+path(
+ 'tag/<slug:tag'
+ '_slug>/', views.post_list, name='post_list_by_tag'
+ ),
  path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
  path('<int:post_id>/share/', views.post_share, name='post_share'),
  path(
